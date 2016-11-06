283ac331cd65463c8d6d19ff27668d2e60af244b
home <home@asus.local.mojatv.ba>
2016-11-06 11:59:05 +0100

    First commit

---
+++ src/ba/ado/edu/ibu/oop/HelloWorld.java
@@ -0,0 +1,10 @@
+package ba.ado.edu.ibu.oop;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
