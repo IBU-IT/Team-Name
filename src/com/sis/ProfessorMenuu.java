3a83f3a03337298f844f4a9312dfd3ecd2bb5745
adnanahmic <adnanahmic203@gmail.com>
2016-12-28 19:33:03 +0100

    additions

--- src/com/sis/ProfessorMenuu.java
+++ src/com/sis/ProfessorMenuu.java
@@ -10,6 +10,8 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ProfessorMenuu extends JFrame {
 
@@ -42,27 +44,34 @@ public class ProfessorMenuu extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnAddStudent = new JButton("Add student");
-		btnAddStudent.setForeground(Color.WHITE);
-		btnAddStudent.setBackground(new Color(148,196,108));
-		btnAddStudent.setBounds(81, 415, 149, 23);
-		frame.getContentPane().add(btnAddStudent);
-		
 		JButton btnSearchStudent = new JButton("Search Student");
 		btnSearchStudent.setForeground(Color.WHITE);
 		btnSearchStudent.setBackground(new Color(148,196,108));
-		btnSearchStudent.setBounds(428, 415, 149, 23);
+		btnSearchStudent.setBounds(427, 415, 150, 23);
 		frame.getContentPane().add(btnSearchStudent);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("C:\\Users\\nixx\\Desktop\\15731437_746703492147708_1514733347_n.jpg"));
-		label.setBounds(0, 0, 621, 512);
-		frame.getContentPane().add(label);
+		JButton btnAddStudent = new JButton("Add Student");
+		btnAddStudent.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnAddStudent.setForeground(Color.WHITE);
+		btnAddStudent.setBackground(new Color(148,196,108));
+		btnAddStudent.setBounds(81, 415, 150, 23);
+		frame.getContentPane().add(btnAddStudent);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\nixx\\Desktop\\15731437_746703492147708_1514733347_n.jpg"));
+		lblNewLabel.setBounds(0, 0, 621, 512);
+		frame.getContentPane().add(lblNewLabel);
+		
+		
+		
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 	}
-
 }
